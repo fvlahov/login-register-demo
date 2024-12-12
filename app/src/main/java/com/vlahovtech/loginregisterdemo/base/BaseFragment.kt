128a18f81541d34933d605f594dd3efadedd8145
fvlahov <Franko.Vlahov@playhera.com>
2024-12-12 14:00:54 +0100

    [REMOVE] Unused imports

--- app/src/main/java/com/vlahovtech/loginregisterdemo/base/BaseFragment.kt
+++ app/src/main/java/com/vlahovtech/loginregisterdemo/base/BaseFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.viewbinding.ViewBinding
-import kotlinx.coroutines.flow.Flow
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
 
